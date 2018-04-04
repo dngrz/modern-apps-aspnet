ee22edc8f86c7e5b809aa8e2c093d1c90be4f726
Clodoaldo Sanchez <dngrz_@hotmail.com>
2018-04-04 09:46:32 -0700

    Ejercicio 1

---
+++ module03/MyShuttle/MyShuttle/MyShuttle/Controllers/CarrierListController.cs
@@ -0,0 +1,35 @@
+﻿using MyShuttle.Data;
+using MyShuttle.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShuttle.Controllers
+{
+    public class CarrierListController : Controller
+    {
+        private ICarrierRepository _carrierRepository;
+        public CarrierListController(ICarrierRepository carrierRepository)
+        {
+            _carrierRepository = carrierRepository;
+        }
+
+        public async Task<ActionResult> Index(SearchViewModel searchVM)
+        {
+            string searchString = searchVM == null ? null : searchVM.SearchString;
+            var carriers = await _carrierRepository.GetCarriersAsync(searchString);
+            var model = new CarrierListViewModel(carriers);
+            return View("Index", model);
+        }
+
+
+        // GET: CarrierList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
